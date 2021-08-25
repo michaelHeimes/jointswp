857f59d0e7f6cf80b68558d7a8363cac607a68ef
Michael Heimes <michael@cairndigitalmedia.com>
2021-08-24 21:00:15 -0400

    added functions

--- functions/disable-gutenberg.php
+++ functions/disable-gutenberg.php
@@ -1,10 +1,14 @@
 <?php
 
 // Disable Gutenberg
+add_filter( 'use_block_editor_for_post', '__return_false' );
+
+// Disable Gutenberg Post Type
+/*
 add_filter('use_block_editor_for_post_type', 'prefix_disable_gutenberg', 10, 2);
 function prefix_disable_gutenberg($current_status, $post_type)
 {
-    // Use your post type key instead of 'product'
     if ($post_type === 'page') return false;
     return $current_status;
 }
+*/
